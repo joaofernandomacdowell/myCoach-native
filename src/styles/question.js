17ba5a8f1457099b483ffe1258d03f3492751417
João Fernando <joaofernando.costa@corp.globo.com>
2017-08-31 21:13:51 -0300

    Add 'Question' component styles

---
+++ src/styles/question.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const question = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 200,
+  },
+  questionTextStyle: {
+    color: '#fff',
+    fontSize: 20
+  }
+});
+
+export default question;
