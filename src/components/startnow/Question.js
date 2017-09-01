894b50ac8b06349eca9ecc850b649ebabbedd8eb
João Fernando <joaofernando.costa@corp.globo.com>
2017-08-31 22:56:29 -0300

    Refactor: Question component

--- src/components/startnow/Question.js
+++ src/components/startnow/Question.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import question from '../../styles/question';
-import { questions } from '../../data/profile-questionnaire';
+import questionStyles from '../../styles/question';
+import { questionsData } from '../../data/profile-questionnaire';
 
 export default class Question extends Component {
   constructor(props) {
@@ -13,14 +13,14 @@ export default class Question extends Component {
 
   componentDidMount() {
     this.setState({
-      question: questions[0].gender
+      question: questionsData[4].time
     });
   }
 
   render() {
     return (
-      <View style={question.container}>
-        <Text style={question.questionTextStyle}>
+      <View style={questionStyles.container}>
+        <Text style={questionStyles.text}>
           {this.state.question}
         </Text>
       </View>
