97babe5e7948b01bbba406108c8803d7aa905f18
João Fernando <joaofernando.costa@corp.globo.com>
2017-09-02 12:56:08 -0300

    Refactor: ButtonOption component

--- src/components/startnow/ButtonOption.js
+++ src/components/startnow/ButtonOption.js
@@ -1,35 +1,13 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
+import buttonOptionStyles from '../../styles/button-option';
 
-const ButtonOption = (props) => {
-  const { buttonStyle, textStyle } = styles;
-
-  return (
-    <TouchableOpacity style={buttonStyle}>
-      <Text style={textStyle}>
-        {props.optionText}
-      </Text>
-    </TouchableOpacity>
-  );
-};
-
-const styles = {
-  textStyle: {
-    textAlign: 'center',
-    justifyContent: 'center',
-    color: '#fff',
-    fontSize: 16,
-    paddingTop: 13,
-    paddingBottom: 10
-  },
-  buttonStyle: {
-    borderWidth: 1,
-    borderColor: '#13F59D',
-    height: 50,
-    width: 250,
-    marginTop: 10,
-    marginBottom: 10
-  }
-};
+const ButtonOption = (props) => (
+  <TouchableOpacity onPress={props.onPress} style={buttonOptionStyles.button}>
+    <Text style={buttonOptionStyles.text}>
+      {props.children}
+    </Text>
+  </TouchableOpacity>
+);
 
 export default ButtonOption;
---
+++ src/styles/button-option.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const buttonOptionStyles = StyleSheet.create({
+  text: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    color: '#fff',
+    fontSize: 16,
+    paddingTop: 13,
+    paddingBottom: 10
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: '#13F59D',
+    height: 50,
+    width: 250,
+    marginTop: 10,
+    marginBottom: 10
+  }
+});
+
+export default buttonOptionStyles;
