66c37d91a5fd00b8ad8cfacd1397a157335fab1c
João Fernando <joaofernando.costa@corp.globo.com>
2017-09-08 23:39:11 -0300

    Update SelectionReducer with correct payload

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,8 +1,19 @@
-export default (state = null, action) => {
+import {
+  SELECTED_OPTION
+} from '../actions/types';
+
+const INITIAL_STATE = {
+  question: '',
+  options: [],
+  profile: {}
+};
+
+export default (state = INITIAL_STATE, action) => {
 
   switch (action.type) {
-    case 'select_option':
-      return { ...state, optionSelected: action.payload };
+    case SELECTED_OPTION:
+      debugger;
+      return { ...state, profile: {[action.payload.type]: action.payload.value}};
     default:
       return state;
   }
