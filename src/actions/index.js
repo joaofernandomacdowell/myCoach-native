6d3486c889c7f5f7509bb797e4e32b539a5e3fe2
João Fernando <joaofernando.costa@corp.globo.com>
2017-09-08 23:38:41 -0300

    Action: change payload of select_option & add 'types' file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,11 @@
+import {
+  SELECTED_OPTION
+} from './types';
+
 // props (this.props.selectOption)
-export const selectOption = (option) => {
+export const selectOption = ({ type, option }) => {
   return {
-    type: 'select_option',
-    payload: option
+    type: SELECTED_OPTION,
+    payload: { type, option }
   };
 };
---
+++ src/actions/types.js
@@ -0,0 +1,1 @@
+export const SELECTED_OPTION = 'selected_option';
