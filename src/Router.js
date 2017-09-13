7223ddf6dd4b6e54aa55a69913a663118a2c653e
João Fernando <joaofernando.costa@corp.globo.com>
2017-09-13 19:40:41 -0300

    Add 'Presentation' to a Router Scene and set initial

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Scene, Stack, Router, Actions } from 'react-native-router-flux';
+
 import RegisterForm from './components/startnow/RegisterForm';
 import QuestionSection from './components/startnow/QuestionSection';
-
+import Presentation from './components/presentation/Presentation';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ flex: 1, backgroundColor: '#153041' }}>
       <Stack key="root">
-        <Scene initial hideNavBar={true} key="questionSection" component={QuestionSection} />
+        <Scene initial hideNavBar={true} key="presentation" component={Presentation} />
+        <Scene hideNavBar={true} key="questionSection" component={QuestionSection} />
         <Scene hideNavBar={true} key="registerForm" component={RegisterForm} />
       </Stack>
     </Router>
